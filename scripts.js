c6041a76b6da18c04c84ffe40892fa4cc21e7395
Home <Home@Homes-MacBook-Pro.local>
2016-10-01 10:09:15 -0500

    completed hw

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 function problemOne() {
   //Get all the numbers from the form.
   let num1 = parseFloat(document.getElementById('num1').value);
+  let num2 = parseFloat(document.getElementById('num2').value);
+  let num3 = parseFloat(document.getElementById('num3').value);
+  let num4 = parseFloat(document.getElementById('num4').value);
+  let num5 = parseFloat(document.getElementById('num5').value);
+  let num6 = parseFloat(document.getElementById('num6').value);
 
   //Find the average, not including the lowest score.
+  var sum = num1 + num2 + num3 + num4 + num5 + num6;
+  findMin(num1, num2, num3, num4, num5,num6);
+  sum -= findMin(num1, num2, num3, num4, num5,num6);
 
+  var average = sum / 5;
   //Print it to the console.
   console.log(average);
 }
@@ -11,14 +20,25 @@ function problemOne() {
 function problemTwo() {
   //Get all the letter grades from the form.
   let grade1 = document.getElementById('grade1').value;
+  let grade2 = document.getElementById('grade2').value;
+  let grade3 = document.getElementById('grade3').value;
+  let grade4 = document.getElementById('grade4').value;
+  let grade5 = document.getElementById('grade5').value;
+  let grade6 = document.getElementById('grade6').value;
 
   //Compute the GPA.
+  var gpa = findGPA(grade1, grade2, grade3, grade4, grade5, grade6);
 
   //Print it to the console.
-
+  console.log(GPA);
   //If it is 3.0 or above, turn the div with id="box" green. Otherwise, turn it red.
   // document.getElementById("box").style.backgroundColor = "orange";
-
+  if (gpa >= 3) {
+    document.getElementById("box").style.backgroundColor = "orange";
+  }
+  else {
+    document.getElementById("box").style.backgroundColor = "red";
+  } 
 }
 
 function findMin(num1, num2, num3, num4, num5, num6) {
